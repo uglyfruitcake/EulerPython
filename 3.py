54c32bcb4ffde04112be0524c494aa1afb984eea
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-04-09 17:22:31 +0100

    Completes project 3

--- 3.py
+++ 3.py
@@ -1,1 +1,11 @@
 import my_module
+
+number = 600851475143
+primes = my_module.generate_prime(number/2)
+prime_factors = []
+
+for i in primes:
+    if number % i == 0:
+        prime_factors.append(i)
+
+print max(prime_factors)
