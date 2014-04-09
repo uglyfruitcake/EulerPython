e3049e9a998e3cdceaf3844888024e854f7a87ec
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-04-09 17:38:23 +0100

    Completes project 4

---
+++ 4.py
@@ -0,0 +1,8 @@
+import my_module
+palindromes = []
+for a in range(100, 1000):
+    for b in range(100, 1000):
+        number = a*b
+        if my_module.is_palindrome(number):
+            palindromes.append(number)
+print max(palindromes)
