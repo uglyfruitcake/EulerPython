68b67151c310e7bf981ce51717b349e2317e25b6
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-04-09 15:43:55 +0100

    solves project 1

--- 1.py
+++ 1.py
@@ -1,3 +1,9 @@
 import my_module
-
-print my_module.is_multiple_of_x(4, 2)
+sum_of_multiples = 0
+for i in xrange(3, 1000):
+    if my_module.is_multiple_of_x(i, 3):
+        sum_of_multiples += i
+    else:
+        if my_module.is_multiple_of_x(i, 5):
+            sum_of_multiples += i
+print sum_of_multiples
