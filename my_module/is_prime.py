b0748943a1b835fbc9d924c4b74621ce1c230291
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-04-10 10:01:15 +0100

    improves is_prime method

--- my_module/is_prime.py
+++ my_module/is_prime.py
@@ -4,6 +4,8 @@ import math
 def is_prime(n):
     if n == 2:
         return True
+    elif n == 1:
+        return False
     for i in range(2, int(math.ceil(math.sqrt(n)))+1):
         if n % i == 0:
             return False
