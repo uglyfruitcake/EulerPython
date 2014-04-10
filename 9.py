c15d8c8d00a79bf5ecc78d60aecc7742253b642e
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-04-10 12:39:16 +0100

    Completes project 9

--- 9.py
+++ 9.py
@@ -1,1 +1,10 @@
+answer = 0
+while answer == 0:
+    for a in xrange(1, 1000):
+        for b in xrange(1, 1000):
+            for c in xrange(1, 1000):
+                if a ** 2 + b ** 2 == c ** 2:
+                    if a + b + c == 1000:
+                        answer = a * b * c
 
+print answer
