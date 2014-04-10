187a906c08ea2080b008e23d84c8617c0325126a
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-04-10 09:04:53 +0100

    Creates project 5

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@ import my_module
 palindromes = []
 for a in range(100, 1000):
     for b in range(100, 1000):
-        number = a*b
+        number = a * b
         if my_module.is_palindrome(number):
             palindromes.append(number)
 print max(palindromes)
