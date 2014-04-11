5aa28136841598c238f39fb0b5dcf8cadd3f7456
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-04-11 20:38:53 +0100

    Anaconda changes

--- my_module/nth_prime.py
+++ my_module/nth_prime.py
@@ -1,9 +1,11 @@
 import my_module
+
+
 def nth_prime(n):
     number = 0
     possible_prime = 1
     while number < n:
         if my_module.is_prime(possible_prime):
             number += 1
-        possible_prime += 1          
+        possible_prime += 1
     return possible_prime - 1
