657b0394f2b7eb857aaf4df157b05fcc336ef856
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-07-20 12:30:05 +0100

    Completes problem 21

---
+++ 21.py
@@ -0,0 +1,8 @@
+import my_module
+
+amicable_numbers = []
+for i in xrange(1, 10000):
+    if my_module.is_amicable_number(i):
+        amicable_numbers.append(i)
+        print i
+print sum(amicable_numbers)
