b3ca8bb36013f9636c53a9995551a4dfa61e77a8
Thomas Campbell <thomas.campbell@tanti.org.uk>
2014-07-20 12:29:53 +0100

    Fixes is amicable number

--- my_module/is_amicable_number.py
+++ my_module/is_amicable_number.py
@@ -4,6 +4,9 @@ import my_module
 def is_amicable_number(a):
     b = sum(my_module.get_proper_divisors(a))
     if a == sum(my_module.get_proper_divisors(b)):
-        return True
+        if a == b:
+            return False
+        else:
+            return True
     else:
         return False
