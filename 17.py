a2b96f776f9578f07b82007a57adc6b762ee79b5
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-04-20 11:48:26 +0100

    Completes Problem 17

---
+++ 17.py
@@ -0,0 +1,34 @@
+units = ["", "one", "two", "three", "four", "five", "six",
+         "seven", "eight", "nine", "ten", "eleven", "twelve",
+         "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+         "eighteen", "nineteen"]
+tens = ["twenty", "thirty", "forty", "fifty",
+        "sixty", "seventy", "eighty", "ninety"]
+total_length = 0
+
+for i in xrange(1, 1001):
+    if i < 20:
+        total_length += len(units[i])
+    elif i < 100:
+        digits = []
+        for digit in str(i):
+            digits.append(int(digit))
+        total_length += len(tens[digits[0]-2])
+        total_length += len(units[digits[1]])
+    else:
+        digits = []
+        for digit in str(i):
+            digits.append(int(digit))
+        if i == 1000:
+            total_length += 11
+        elif i % 100 == 0:
+            total_length += (len(units[digits[0]])+7)
+        elif i - digits[0]*100 < 20:
+            total_length += len(units[i - digits[0]*100])
+            total_length += len(units[digits[0]]) + 10
+        else:
+            total_length += len(tens[digits[1]-2])
+            total_length += len(units[digits[2]])
+            total_length += len(units[digits[0]]) + 10
+
+print(total_length)
--- 17NC.py
+++
@@ -1,4 +0,0 @@
-import my_module
-
-
-print my_module.problem_not_completed(17)
