3eaa55d1730dd4bcae85b111952d6abd5dd08374
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-04-20 17:22:24 +0100

    Improves proper divisors method

--- my_module/get_proper_divisors.py
+++ my_module/get_proper_divisors.py
@@ -3,7 +3,7 @@ import my_module
 
 def get_proper_divisors(n):
     divisors = [1]
-    for i in xrange(2, n/2):
+    for i in xrange(2, n/2 + 1):
         if my_module.is_factor(i, n):
             divisors.append(i)
     return divisors
