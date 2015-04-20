efcdb7df753c66f385218f535013dcd5a5732bf3
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-04-20 16:43:40 +0100

    Fixes triangular number method

--- my_module/generate_triangular_numbers.py
+++ my_module/generate_triangular_numbers.py
@@ -2,7 +2,7 @@ def generate_triangular_numbers(ceiling):
     number = 1
     triangular_number = 0
     triangular_numbers = []
-    while triangular_number <= ceiling:
+    while triangular_number < ceiling:
         triangular_number += number
         triangular_numbers.append(triangular_number)
         number += 1
