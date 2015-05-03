21654a38a5a693d8516fe9151c83d8ac54e9b517
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-05-03 14:39:23 +0100

    Fixes pep8 errors

--- my_module/generate_abundant_number.py
+++ my_module/generate_abundant_number.py
@@ -4,7 +4,6 @@ import my_module
 def generate_abundant_number(ceiling):
     abundant_numbers = []
     for i in xrange(12, ceiling + 1):
-        divisors = my_module.get_proper_divisors
-        if sum(my_module.get_proper_divisors(i))>i:
+        if sum(my_module.get_proper_divisors(i)) > i:
             abundant_numbers.append(i)
     return(abundant_numbers)
