dac54950ba0926950731d1f39994707af2be9158
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-06-30 17:11:02 +0100

    Fixes output errors

--- 27.py
+++ 27.py
@@ -3,7 +3,6 @@ import my_module
 maxprimes = 0
 product = 0
 for a in xrange(-1000, 1001):
-    print(a)
     for b in xrange(-1000, 1001):
         if my_module.consecutive_primes_from_quadratic(a, b) > maxprimes:
             maxprimes = my_module.consecutive_primes_from_quadratic(a, b)
