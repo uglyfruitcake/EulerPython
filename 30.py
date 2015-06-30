2d456c73e688462b0d98ce968ca253efb5a9165e
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-06-30 19:19:43 +0100

    Completes problem 30

---
+++ 30.py
@@ -0,0 +1,10 @@
+import my_module
+answer = 0
+for i in xrange(10, 1000000):
+    total = 0
+    for digit in my_module.array_of_digits(i):
+        total += digit ** 5
+    if total == i:
+        answer += i
+
+print(answer)
