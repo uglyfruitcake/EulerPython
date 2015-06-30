d8d156bcd7382ba41a57cf47639f71f3a3efd891
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-06-30 16:31:32 +0100

    Fixes pep8 errors

--- my_module/cycle_length.py
+++ my_module/cycle_length.py
@@ -1,6 +1,6 @@
 def cycle_length(n):
-    for i in range(1, n+1):
-        if ((10**i)-1) % n == 0:
+    for i in range(1, n + 1):
+        if ((10 ** i)-1) % n == 0:
             return(i)
             break
         else:
