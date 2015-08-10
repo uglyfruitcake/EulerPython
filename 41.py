1ddaf32f6c4784be9b2b75bb16fcb4201461fa6a
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-08-10 16:17:36 +0100

    Fixes problem 41 output

--- 41.py
+++ 41.py
@@ -7,6 +7,5 @@ intpermutations = [int("".join(i)) for i in permutations]
 primes = []
 for n in intpermutations:
     if my_module.is_prime(n):
-        print(n)
         primes.append(n)
 print(max(primes))
