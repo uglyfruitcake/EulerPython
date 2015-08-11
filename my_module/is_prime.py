ca83dae6926b06a1610ced05714228833ca1491c
Thomas Campbell <thomas.campbell@tanti.org.uk>
2015-08-11 10:39:46 +0100

    Improves is prime function

--- my_module/is_prime.py
+++ my_module/is_prime.py
@@ -6,7 +6,7 @@ def is_prime(n):
         return True
     elif n == 1:
         return False
-    for i in range(2, int(math.ceil(math.sqrt(n)))+1):
+    for i in range(2, int((n ** 0.5) + 1)):
         if n % i == 0:
             return False
     return True
